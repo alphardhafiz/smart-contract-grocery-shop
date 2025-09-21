a2011a768021ac4442f6281d9a74c692a92c2fa9
alphardhafiz <bucialphard@gmail.com>
2025-09-21 17:47:49 +0700

    progress min 22:11

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
-// time 12:49
+// time 22:11
 contract GroceryShop {
     address public owner;
     uint256 public purchaseId; // id counter
@@ -31,5 +31,17 @@ contract GroceryShop {
         groceryItem[GroceryType.Egg] = Grocery("Telur", 10);
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call function.");
+        _;
+    }
 
+    modifier numberChecking(uint256 number) {
+        require(number > 0, "Must be at least 1");
+        _;
+    }
+
+    function add(GroceryType _groceryType, uint _numberAdded) public onlyOwner numberChecking(_numberAdded) {
+        groceryItem[_groceryType].numberOfItems += _numberAdded;
+    }
 }
